9790b08c3f96e6c66330c3df540d526276c2b673
druidmentalized <dmitry.barmuta@gmail.com>
2025-05-10 02:46:51 +0200

    Improved DTO, for enhanced data validation

--- Tutorial8/Models/Client/CreateClientDTO.cs
+++ Tutorial8/Models/Client/CreateClientDTO.cs
@@ -1,10 +1,24 @@
 namespace Tutorial8.Models.Client;
 
+using System.ComponentModel.DataAnnotations;
+
 public class CreateClientDTO
 {
+    [Required]
+    [StringLength(120)]
     public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(120)]
     public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+
+    [Phone]
     public string Telephone { get; set; }
+
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "PESEL must be 11 characters")]
     public string Pesel { get; set; }
 }
